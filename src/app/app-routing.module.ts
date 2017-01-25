31860bf9185f4e575312c2adc81d0045bd6fa116
Peter Miklos <pt.miklos@gmail.com>
2017-01-25 14:22:47 +0000

    edit link added to search.component, app-routing updated with edit path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes }   from '@angular/router';
 
 import { SearchComponent }        from './search/search.component';
+import { EditComponent }          from './edit/edit.component';
 
 const routes: Routes = [
   {path: 'search', component: SearchComponent },
+  {path: 'edit/:id', component: EditComponent },
   {path: '', redirectTo: '/search', pathMatch: 'full' }
 ]
 
