c9cc89278df7c0f591539b1b7b81fdf884824464
Peter Miklos <pt.miklos@gmail.com>
2017-02-03 22:45:40 +0000

    searchService.search related test cases added

--- src/app/_services/search.service.spec.ts
+++ src/app/_services/search.service.spec.ts
@@ -45,13 +45,25 @@ describe('SearchService', () => {
   }));
 
   // describe('#getAll', () => {
+  //   beforeEach(() => {
+  //     spyOn(httpModule, 'get').and.returnValue(Promise.resolve(persons));
+  //   })
   //
-  // }));
+  //   it('trial', done => {
+  //     service.getAll().then(value => {
+  //       expect(value).toBe(persons);
+  //       done();
+  //     })
+  //   })
+  //
+  //   xit('calls http.get method with a link', done => {
+  //
+  //   })
+  // });
 
   describe('#getPerson', () => {
 
     beforeEach(() => {
-      // service = new SearchService();
       spyOn(service, 'getAll').and.returnValue(Promise.resolve(persons));
     });
 
@@ -74,4 +86,58 @@ describe('SearchService', () => {
       })
     })
   });
+
+  describe('#search', () => {
+
+    beforeEach(() => {
+      spyOn(service, 'getAll').and.returnValue(Promise.resolve(persons));
+    });
+
+    it('returns all persons if no value is used in search', done => {
+      service.search('').then(value => {
+        expect(value.length).toBe(2);
+        expect(value[0]).toBe(persons[0]);
+        expect(value[1]).toBe(persons[1]);
+        done();
+      })
+    })
+
+    it('returns all persons if a * is used in search', done => {
+      service.search('*').then(value => {
+        expect(value.length).toBe(2);
+        expect(value[0]).toBe(persons[0]);
+        expect(value[1]).toBe(persons[1]);
+        done();
+      })
+    })
+
+    it('finds and returns one person if first name is searched', done => {
+      service.search('jim').then(value => {
+        expect(value.length).toBe(1);
+        expect(value[0]).toBe(persons[1]);
+        done();
+      })
+    })
+
+    it('finds and returns both people if city is searched (same city included)', done => {
+      service.search('Myrtle Beach').then(value => {
+        expect(value.length).toBe(2);
+        expect(value[0]).toBe(persons[0]);
+        expect(value[1]).toBe(persons[1]);
+        done();
+      })
+    })
+
+    it('is not case sensitive', done => {
+      service.search('bOb sMiT').then(value => {
+        expect(value.length).toBe(1);
+        expect(value[0]).toBe(persons[0]);
+        done();
+      })
+    })
+  });
+
+  describe('#save', () => {
+
+  })
 });
