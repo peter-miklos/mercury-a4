04081c2a975e73f014ea9cfa4bbacff7c4dc5878
Peter Miklos <pt.miklos@gmail.com>
2017-02-05 20:14:16 +0000

    person.model added, search.service updated

--- src/app/_services/search.service.ts
+++ src/app/_services/search.service.ts
@@ -62,19 +62,19 @@ export class SearchService {
   }
 }
 
-export class Person {
-  id: number;
-  name: string;
-  phone: string;
-  address: Address;
-
-  constructor(obj?: any) {
-    this.id = obj && Number(obj.id) || null;
-    this.name = obj && obj.name || '';
-    this.phone = obj && obj.phone || '';
-    this.address = obj && obj.address || '';
-  }
-}
+// export class Person {
+//   id: number;
+//   name: string;
+//   phone: string;
+//   address: Address;
+//
+//   constructor(obj?: any) {
+//     this.id = obj && Number(obj.id) || null;
+//     this.name = obj && obj.name || '';
+//     this.phone = obj && obj.phone || '';
+//     this.address = obj && obj.address || '';
+//   }
+// }
 
 // export class Address {
 //   street: string;
