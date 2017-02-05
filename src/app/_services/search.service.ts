7e20876b1f6aa46487502c972026b0caa9274d84
Peter Miklos <pt.miklos@gmail.com>
2017-02-05 20:11:49 +0000

    address model added, search.service updated

--- src/app/_services/search.service.ts
+++ src/app/_services/search.service.ts
@@ -76,16 +76,16 @@ export class Person {
   }
 }
 
-export class Address {
-  street: string;
-  city: string;
-  state: string;
-  zip: string;
-
-  constructor(obj?: any) {
-    this.street = obj && obj.street || '';
-    this.city = obj && obj.city || '';
-    this.state = obj && obj.state || '';
-    this.zip = obj && obj.zip || '';
-  }
-}
+// export class Address {
+//   street: string;
+//   city: string;
+//   state: string;
+//   zip: string;
+//
+//   constructor(obj?: any) {
+//     this.street = obj && obj.street || '';
+//     this.city = obj && obj.city || '';
+//     this.state = obj && obj.state || '';
+//     this.zip = obj && obj.zip || '';
+//   }
+// }
