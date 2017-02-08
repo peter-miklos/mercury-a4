56ff3f9c082fcfa6375595346e909b6a046bee24
Peter Miklos <pt.miklos@gmail.com>
2017-02-08 23:02:00 +0000

    first e2e tests added

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,30 @@
-import { MercuryA4Page } from './app.po';
+import { MercuryA4Page }        from './app.po';
+import { browser, element, by } from 'protractor';
 
-describe('mercury-a4 App', function() {
+describe('mercury-a4 App', () => {
   let page: MercuryA4Page;
 
   beforeEach(() => {
     page = new MercuryA4Page();
   });
 
-  it('should display message saying app works', () => {
+  it("displays the app title in the toolbar", () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(element(by.css("span#title")).getText()).toEqual("mercury-a4");
+  });
+
+  it("displays search md icon in the toolbar", () => {
+    page.navigateTo();
+    expect(element(by.css("md-icon#search")).getText()).toEqual('search');
+  })
+
+  it("navigates to search page if search icon is clicked", () => {
+    element(by.css("a")).click();
+    expect(browser.getCurrentUrl()).toEqual("http://localhost:4200/search");
+  })
+
+  xit('should display Search in the header', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Search');
   });
 });
